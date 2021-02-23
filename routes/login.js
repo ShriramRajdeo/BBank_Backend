017a4bf18325502a0aaf56ad21cb736a6ff89cb7
ShriramRajdeo <rajdeoshriram@gmail.com>
2021-02-23 15:32:58 +0530

    Complete signup with correction

--- routes/login.js
+++ routes/login.js
@@ -49,7 +49,7 @@ router.post("/",function(req,res){
     
         var sqltable = "CREATE TABLE IF NOT EXISTS userData( userId int NOT NULL AUTO_INCREMENT, name varchar(40) NOT NULL, emailId varchar(40) NOT NULL,password varchar(40) NOT NULL,mobile varchar(10) NOT NULL,dob varchar(10) NOT NULL,gender varchar(15) NOT NULL,bloodGr varchar(15) NOT NULL,pincode varchar(6) NOT NULL,PRIMARY KEY (userId),FOREIGN KEY (pincode) REFERENCES location(pincode));";
     
-        mysqlConnection.query(sqltable,[userDetail],function (err, data) { 
+        mysqlConnection.query(sqltable,function (err, data) { 
             if (err) throw err;
             console.log("UserData table created successfully "); 
         });
@@ -58,7 +58,7 @@ router.post("/",function(req,res){
         // Create location table if not exists
         var sql = 'CREATE TABLE IF NOT EXISTS location(pincode varchar(6) NOT NULL UNIQUE,city varchar(30) NOT NULL,state varchar(30) NOT NULL,country varchar(30) NOT NULL,PRIMARY KEY (pincode));';
         
-        mysqlConnection.query(sql,[userDetail],function (err, data) { 
+        mysqlConnection.query(sql,function (err, data) { 
             if (err) throw err;
             console.log("location table created successfully "); 
         });
@@ -92,7 +92,7 @@ router.post("/",function(req,res){
         // Create location table if not exists
         var sql = 'CREATE TABLE IF NOT EXISTS location(pincode varchar(6) NOT NULL UNIQUE,city varchar(30) NOT NULL,state varchar(30) NOT NULL,country varchar(30) NOT NULL,PRIMARY KEY (pincode));';
 
-        mysqlConnection.query(sql,[locationData],function (err, data) { 
+        mysqlConnection.query(sql,function (err, data) { 
             if (err) throw err;
             console.log("location table created successfully "); 
         });
@@ -109,7 +109,7 @@ router.post("/",function(req,res){
         // Create bloodBankData table if not exists
         var sql = 'CREATE TABLE IF NOT EXISTS bloodBankData(bankId int NOT NULL AUTO_INCREMENT, name varchar(40) NOT NULL, emailId varchar(40) NOT NULL, password varchar(40) NOT NULL, mobile varchar(10) NOT NULL, pincode varchar(6) NOT NULL,PRIMARY KEY (bankId), FOREIGN KEY (pincode) REFERENCES location(pincode));';
 
-        mysqlConnection.query(sql,[bankDetail],function (err, data) { 
+        mysqlConnection.query(sql,function (err, data) { 
             if (err) throw err;
             console.log("bloodBankData table created successfully "); 
         });
