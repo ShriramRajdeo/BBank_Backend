432dd173ab42dba80d6a04c1ef3bbb34842761fa
yash gandhi <yashgandhi2020@gmail.com>
2021-05-29 14:48:52 +0530

    small changes in auth

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,11 +2,12 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function checkAuth(req, res, next){
     try{
-        const token = req.headers.authorization.split(" ")[1]; 
+        const token = req.headers.authorization.split(" ")[1];
         const decodedToken = jwt.verify(token, process.env.JWT_KEY);
         req.body.data = decodedToken;
         next();
     }catch(e){
+        console.log(e.message);
         return res.status(401).json({
             'message': "Invalid or expired token provided!",
             'error':e
